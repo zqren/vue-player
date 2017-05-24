b1f52b460a87a2e360899078f8f59e190651d3bd
lxrcshik@sina.com <really**126>
2017-05-24 13:20:24 +0800

    test

--- src/components/homePage/playlist-song.vue
+++ src/components/homePage/playlist-song.vue
@@ -4,8 +4,6 @@
         <div class="hot-playlist-label" :class="{topBorder:isBorder,fixed:isFixed}">
             <span class="playlist">{{label}}</span>
             <span class="arrow"></span>
-
-            
         </div>
         <!--歌单列表-->
         <div class="hot-playlist-detail">
